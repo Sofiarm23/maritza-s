32518cfe7e0e7e9dfd7f8a9d548eb2c6344d18b1
caroline ruiz <caroline_ruiz@usmp.pe>
2020-10-20 18:00:00 -0500

    maritza's v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace maritza_s.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
